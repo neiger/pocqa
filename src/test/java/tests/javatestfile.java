f467ac096b3cf9d306439f1b14d1cc33795cb6fb
neiger <neiger.serrano.wks@gmail.com>
2023-04-12 13:25:04 -0600

    git issue fixed

--- src/test/java/tests/javatestfile.java
+++ src/test/java/tests/javatestfile.java
@@ -4,6 +4,7 @@ public class javatestfile {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");   
+        System.out.println("The git issue is fixed");
     }
     
 }
